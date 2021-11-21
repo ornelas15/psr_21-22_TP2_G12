e9a59fc77e320e522130aeb3d0f0b1683cff1170
Pedro Amaral <pedromiglou@hotmail.com>
2021-11-21 14:16:05 +0000

    fa 4 e 5 a funcionar com rato

--- ar_paint.py
+++ ar_paint.py
@@ -1,12 +1,53 @@
 #!/usr/bin/python3
 import numpy as np
 import cv2
+import argparse
 
 clicking = False
 color = (0, 0, 255) # BGR
-painting = np.ones((565, 718, 3)) * 255  # start with everything white
+painting = np.ones((550, 700, 3)) * 255  # start with everything white
 
 
+# obtain a numbered inverted image, labels and label-color matches
+def load_coloring_image():
+    cImage = cv2.imread("./images/ovni.png", cv2.IMREAD_GRAYSCALE)
+    
+    ret, thresh = cv2.threshold(cImage,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    # use connectedComponentWithStats to find the white areas
+    connectivity = 4
+    output = cv2.connectedComponentsWithStats(thresh, connectivity, cv2.CV_32S)
+
+    num_labels = output[0] # number of labels / areas
+    labels = output[1] # label matrix
+    stats = output[2] # statistics
+    centroids = output[3] # centroid matrix
+    
+    # associate a label with a color
+    colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0)]
+    labelColors = dict()
+    
+    height, width = cImage.shape
+    for i in range(height):
+        for j in range(width):
+            if labels[i][j] not in labelColors.keys():
+                if thresh[i][j] == 0:
+                    labelColors[labels[i][j]] = (0, 0, 0)
+                else:
+                    labelColors[labels[i][j]] = colors[labels[i][j]%3]
+    
+    # write the numbers on the image
+    fontScale = (width * height) / (800 * 800)
+    for i in range(0, len(centroids)):
+        if labelColors[i] != (0,0,0):
+            cv2.putText(cImage,str(i), (int(centroids[i][0]-fontScale*15), int(centroids[i][1]+fontScale*15)), cv2.FONT_HERSHEY_SIMPLEX, fontScale, (0,0,0), 1)
+
+    cImage = cv2.bitwise_not(cImage)
+    
+    return cv2.cvtColor(cImage,cv2.COLOR_GRAY2RGB), labelColors, labels
+
+
+# deal with mouse events
 def mouse_paint(event, x, y, flags, params):
     global clicking, painting
     
@@ -20,57 +61,44 @@ def mouse_paint(event, x, y, flags, params):
         clicking = False
 
 
-# funcionalidade avancada 4
-def load_coloring_image():
-    cImage = cv2.imread( "./images/ovni.png", cv2.IMREAD_GRAYSCALE)
-    
-    _, cImage = cv2.threshold(cImage, 128, 255, cv2.THRESH_BINARY_INV)
-    
-    height, width = cImage.shape
-    
-    areas = set()
-    for i in range(0, height, 20):
-        for j in range(0, width, 20):
-            if cImage[i][j] == 0:
-                image2 = cImage.copy().astype("uint8")
-
-                mask = np.zeros((height+2, width+2), np.uint8)
-                try:
-                    cv2.floodFill(image2, mask, (j,i), 255, 10, 10)
-                except:
-                    print(i)
-                    print(j)
-                
-                image2 = cv2.bitwise_not(image2)
-                areas.add(tuple(image2.reshape((height*width))))
-    
-    print(len(areas))
-    cv2.imshow('Image after Flood Fill with seed 430x30', cImage | image2)
-
-
 def main():
     # -----------------------------------
     # Initialize
     # -----------------------------------
-    global painting, color
+    global painting, color, labels
+    
+    # Define argparse inputs
+    parser = argparse.ArgumentParser(description='Definition of test mode')
+    parser.add_argument('-cim','--coloring_image_mode', action='store_true', help='If present, it will presented a coloring image to paint.', required=False)
+
+    # Parse arguments
+    args = parser.parse_args()
     
     window_name = "Augmented Reality Paint"
     print(window_name)
-
-    # funcionalidade avancada 4
-    #image = load_coloring_image()
     
-    cv2.imshow(window_name, painting)
-    painting = painting.astype(np.uint8)
+    # coloring image mode
+    if args.coloring_image_mode:
+        cImage, labelColors, labels = load_coloring_image()
+        height, width, _ = cImage.shape
+        painting = np.ones((height, width, 3)) * 255
 
+        cv2.imshow(window_name, cv2.subtract(painting, cImage, dtype=cv2.CV_64F))
+    else:
+        cv2.imshow(window_name, painting)
+        
     cv2.setMouseCallback(window_name, mouse_paint)
 
     # -----------------------------------
     # Execution
     # -----------------------------------
     while True:
-        cv2.imshow(window_name, painting)
+        if args.coloring_image_mode:
+            cv2.imshow(window_name, cv2.subtract(painting, cImage, dtype=cv2.CV_64F))
+        else:
+            cv2.imshow(window_name, painting)
 
+        # deal with keyboard events
         key = cv2.waitKey(20)
         if key != -1:
             print("Tecla pressionada: " + str(key))
@@ -84,6 +112,19 @@ def main():
                 color = (255, 0, 0)
                 print('Blue color selected')
             elif key == ord('Q') or key == ord('q') or key == 27: # 27 -> ESC
+                if args.coloring_image_mode:
+                    hits = 0
+                    misses = 0
+                    for i in range(height):
+                        for j in range(width):
+                            rightColor = labelColors[labels[i][j]]
+                            if np.array_equal(painting[i][j], rightColor):
+                                hits += 1
+                            elif rightColor != (0, 0, 0):
+                                misses += 1
+                
+                    print(hits)
+                    print(hits/(hits+misses))
                 print("Quitting")
                 break
     
